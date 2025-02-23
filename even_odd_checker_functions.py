8cf2e351ccaa5bd807b6f433ff6b4677b961e779
BSIT Dela Cruz <cathydelacruz010@gmail.com>
2025-02-24 00:48:01 +0800

    Submitting Python Week 05 - Laboratory # 20

---
+++ even_odd_checker_functions.py
@@ -0,0 +1,7 @@
+user_input = input("Enter an Integer: ")
+number = int(user_input)
+number = int(user_input)
+if number % 2 == 0:
+    print(number, "is an Even number.")
+else:
+    print(number, "is an Odd number.")
